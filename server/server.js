9162333ae2e7eb242763ab234314f4dde5b53431
dfuruya <dfuruya@gmail.com>
2016-06-01 18:04:57 -0700

    Implement POST request with Watson integration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
-// var http = require('http').Server(app);
 var bodyParser = require('body-parser');
+var watson = require('watson-developer-cloud');
 // var io = require('socket.io')(http);
 var db = require('./db.js');
+var configFile = require('../config/config.js');
+
+var tone_analyzer = watson.tone_analyzer({
+  username: configFile.WATSON_USERNAME,
+  password: configFile.WATSON_PASSWORD,
+  version: 'v3',
+  version_date: '2016-05-19'
+});
 
 db.connect(function(err) {
   if (err) { console.log('connection error: ', err); }
@@ -16,11 +24,25 @@ app.use(bodyParser.json());
 app.get('/messages', function(req, res) {
   db.query('select * from users', function (err, results){
     if (err) {
-      console.log(err);
-      return;
+      console.error(err);
+    } else {
+      res.json(results);
     }
-    res.json(results);
   });
+});
+
+// handle post requests
+app.post('/messages', function(req, res) {
+  console.log("request has been sent: ", req.body);
+  tone_analyzer.tone({ text: req.body.message },
+    function(err, tone) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(tone);
+      }
+    }
+  );
 });
 
 var port = process.env.PORT || 8080;

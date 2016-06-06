123fee015fea7d5a41718269edd9f762d7023e30
Arthur Livingston <arthurlivingston@Arthurs-MacBook-Air.local>
2016-06-05 18:16:54 -0700

    update background rendering

--- app/components/Chats.jsx
+++ app/components/Chats.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Chats = props => (
-  <div>
+  <div className='form-horizontal'>
     <form onSubmit={ e => props.sendMessage(e) }>
-      <textarea value={ props.currentChat.inputText } onChange={ e => props.handleMessageChange(e) } />
-      <input type="submit" />
+      <textarea value={ props.currentChat.inputText } onChange={ e => props.handleMessageChange(e) } className='form-control' rows='3' />
+      <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
     </form>
   </div>
 );

50bc20f9782ada67e74f15969f79220ef5d94b46
dfuruya <dfuruya@gmail.com>
2016-06-06 10:37:31 -0700

    Add pointers to background images in wallpaperdata.js

--- app/data/wallpaperdata.js
+++ app/data/wallpaperdata.js
@@ -1,10 +1,10 @@
 var images = {
   emotions: {
-  Anger: 'http://royalpurplenews.com/wp-content/uploads/2012/03/Anger-Management.jpg',
-  Disgust:'http://pre00.deviantart.net/7c4d/th/pre/i/2010/007/a/4/the_sickening___print_by_phlegeton.jpg',
-  Fear:'http://themindunleashed.org/wp-content/uploads/2014/08/nightmaree.jpg',
-  Joy:'http://www.artsfon.com/pic/201503/1920x1080/artsfon.com-68037.jpg',
-  Sadness:'http://www.cardloanhikakunavi.com/wp-content/uploads/2015/11/Quotes-help-sadness.jpg',
+  Anger: '../images/anger/Anger-Management-edit.jpg',
+  Disgust: '../images/disgust/Disgust-edit.jpg',
+  Fear: '../images/fear/fear3815-edit.jpg',
+  Joy: '../images/joy/wm-kids-1-edit.jpg',
+  Sadness: '../images/sadness/Sadness-Inside-Out-2015-Film-HD-Wallpaper-edit.jpg',
 },
 social: {
   'Emotional Range': 'http://autismteachingstrategies.com/wp-content/uploads/2013/11/IMG_0899.jpg',
